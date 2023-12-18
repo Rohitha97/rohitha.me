41fc1b496a32c66529fe9de3f28564806875f392
Rohitha Rathnayake <rohith_rathnayake@yahoo.com>
2023-12-18 16:59:57 +0530

    blog count issue fixed

--- app/api/prisma/hitsSlug/route.ts
+++ app/api/prisma/hitsSlug/route.ts
@@ -14,45 +14,45 @@ export async function GET(req: NextRequest) {
 
     let viewCount = 0;
 
-    if (process.env.NODE_ENV !== "development") {
-      // Increment the view count in production
-      const post = await prisma.post.findUnique({
-        where: {
+    // if (process.env.NODE_ENV !== "development") {
+    // Increment the view count in production
+    const post = await prisma.post.findUnique({
+      where: {
+        slug: slug as string,
+      },
+    });
+
+    if (!post) {
+      const newPost = await prisma.post.create({
+        data: {
           slug: slug as string,
+          views: 1,
         },
       });
-
-      if (!post) {
-        const newPost = await prisma.post.create({
-          data: {
-            slug: slug as string,
-            views: 1,
-          },
-        });
-        viewCount = newPost.views;
-      } else {
-        const updatedPost = await prisma.post.update({
-          where: {
-            id: post.id,
-          },
-          data: {
-            views: post.views + 1,
-          },
-        });
-        viewCount = updatedPost.views;
-      }
+      viewCount = newPost.views;
     } else {
-      // Get view count without incrementing in local environment
-      const post = await prisma.post.findUnique({
+      const updatedPost = await prisma.post.update({
         where: {
-          slug: slug as string,
+          id: post.id,
+        },
+        data: {
+          views: post.views + 1,
         },
       });
-
-      if (post) {
-        viewCount = post.views;
-      }
+      viewCount = updatedPost.views;
     }
+    // } else {
+    //   // Get view count without incrementing in local environment
+    //   const post = await prisma.post.findUnique({
+    //     where: {
+    //       slug: slug as string,
+    //     },
+    //   });
+
+    //   if (post) {
+    //     viewCount = post.views;
+    //   }
+    // }
 
     return new Response(JSON.stringify({ Views: viewCount }), {
       status: 200,
